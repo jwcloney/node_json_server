45c0625128b48a6b1021c4645bd3c1e5ce6aaf20
jwcloney <jameswcloney@gmail.com>
2014-10-09 11:53:52 +0100

    small error in home.js

--- app/content/home.js
+++ app/content/home.js
@@ -4,20 +4,26 @@ $(function(){
 
     //init page
     var initPage = function(){
-        $.get("templates/home.html",function(d){
+        $.get("/templates/home.html",function(d){
             tmpl = d;
+            console.log("james here init page  " + tmpl);
         });
 
-    //retrieve server data and init page
-        $.getJSON("albums.json",function(d){
+        //retrieve server data and init page
+        $.getJSON("/albums.json",function(d){
+            console.log("james here tdata " + tdata);
+            console.log("james here d.data " + d.data);
+            console
             $.extend(tdata, d.data);
         });
-    //when ajax is done replace moustache tags
+
+        //when ajax is done replace moustache tags
         $(document).ajaxStop(function(){
             var final_data = massage(tdata);
+            console.log("james here final_data " + final_data);
             var rendered_page = Mustache.to_html(tmpl,final_data);
             $("body").html(rendered_page);
-        });
+        })
     }();
 });
 
